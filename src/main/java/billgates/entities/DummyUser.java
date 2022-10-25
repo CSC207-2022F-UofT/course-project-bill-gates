a34e9f98a7d814e88b8af856ae2e177537a13ad2
Scott <ScottCTD@outlook.com>
2022-10-24 21:41:09 -0400

    [+] Add some initial implementations of entities.

---
+++ src/main/java/billgates/entities/DummyUser.java
@@ -0,0 +1,43 @@
+package billgates.entities;
+
+import java.util.Objects;
+
+public class DummyUser {
+
+    private int id;
+    private String name;
+
+    public DummyUser(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        DummyUser dummyUser = (DummyUser) o;
+        return id == dummyUser.id && this.name.equals(dummyUser.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
