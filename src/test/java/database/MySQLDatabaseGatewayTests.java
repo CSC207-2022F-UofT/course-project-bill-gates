2a66e14c1a4e0d9e194bb797a6b1cab82324a558
HLeiTR <sy.hung@mail.utoronto.ca>
2022-11-15 00:31:29 -0500

    [*] Actually fixing the GitHub automated testing

--- src/test/java/database/MySQLDatabaseGatewayTests.java
+++ src/test/java/database/MySQLDatabaseGatewayTests.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-public class TestMySQLDatabaseGateway {
+public class MySQLDatabaseGatewayTests {
     public MySQLDatabaseGateway testGateway;
     public int testBillID = 9999;
 
