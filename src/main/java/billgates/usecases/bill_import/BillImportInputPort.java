31a3325b3c157ce516a46daca03c054dbc6d6fed
eevadai <daieva030703@gmail.com>
2022-11-21 03:06:40 -0500

    Delete unnecessary files

--- src/main/java/billgates/usecases/bill_import/BillImportInputPort.java
+++
@@ -1,9 +0,0 @@
-package billgates.usecases.bill_import;
-
-import billgates.database.QueryEntryData;
-
-import java.util.List;
-
-public interface BillImportInputPort {
-    void importBill(BillImportRequestModel model);
-}
