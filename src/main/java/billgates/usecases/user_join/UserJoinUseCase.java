e85672ee2386ae6a0189f36558af14b8257da992
CharlotteChenyy <yuhanchen667@gmail.com>
2022-11-21 17:37:17 -0500

    [*] Delete a redundant line

--- src/main/java/billgates/usecases/user_join/UserJoinUseCase.java
+++ src/main/java/billgates/usecases/user_join/UserJoinUseCase.java
@@ -45,6 +45,5 @@ public class UserJoinUseCase implements UserJoinInputPort {
                         "or the username already exists."));
             }
         }
-        // TODO: Call BillUpdateController
     }
 }
