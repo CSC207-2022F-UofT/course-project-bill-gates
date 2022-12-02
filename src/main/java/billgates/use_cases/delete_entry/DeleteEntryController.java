a7792447570cfb1d01ca2ce4aa024f0abb879a0a
Ellen141319 <ellenyy.zhang@mail.utoronto.ca>
2022-12-02 13:43:24 -0500

    [+] Added Javadoc for DeleteEntryUseCase

--- src/main/java/billgates/use_cases/delete_entry/DeleteEntryController.java
+++ src/main/java/billgates/use_cases/delete_entry/DeleteEntryController.java
@@ -2,9 +2,14 @@ package billgates.use_cases.delete_entry;
 
 /**
  * Clean Architecture Layer: Interface Adapters
+ * This class serves as the controller of the Delete Entry Use Case.
+ * It is only responsible for accepting the input (<code> entryID </code>) from the user and
+ * invoke the corresponding use case.
  *
  * @author Ellen, Scott
+ * @see DeleteEntryInputPort
  */
+
 public class DeleteEntryController {
 
     private final DeleteEntryInputPort useCase;
