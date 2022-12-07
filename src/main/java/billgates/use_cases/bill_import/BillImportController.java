5f465f4d99cf38d0ac24c9134abef832f3dd9681
eevadai <daieva030703@gmail.com>
2022-12-07 16:20:53 -0500

    Remove QueryEntryData and change it to be aligned with the current version. Fixed the problem of naming the CSV file.
    Further consideration: Do we need to check for the duplicated value in the import use case?

--- src/main/java/billgates/use_cases/bill_import/BillImportController.java
+++ src/main/java/billgates/use_cases/bill_import/BillImportController.java
@@ -6,7 +6,7 @@ import billgates.use_cases.insert_entry.InsertEntryInputPort;
 /**
  * Clean Architecture Layer: Interface Adapters
  * This class serves as the controller of the Bill Import Use Case.
- * It is only responsible for accepting the input (<code> model </code>) g from the user and
+ * It is only responsible for accepting the input (<code> model </code>) from the user and
  * invoke the corresponding use case.
  *
  * @author Eva
