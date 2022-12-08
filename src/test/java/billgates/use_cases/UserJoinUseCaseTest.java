8358c285d166dc5576652a5ad684cedb83314c53
xgao28 <113380522+xgao28@users.noreply.github.com>
2022-12-08 03:44:10 -0500

    [+] Added initial implementation that needs to be fixed.

---
+++ src/test/java/billgates/use_cases/UserJoinUseCaseTest.java
@@ -0,0 +1,65 @@
+package billgates.use_cases;
+
+import billgates.database.MySQLDatabaseGateway;
+import billgates.entities.User;
+import billgates.interface_adapters.DatabaseGateway;
+import billgates.use_cases.user_join.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A test for <code>UserJoinUseCase</code>.
+ * This test utilizes
+ */
+
+public class UserJoinUseCaseTest {
+
+    private DatabaseGateway gateway = new MySQLDatabaseGateway();
+    private UserJoinController controller;
+    private UserJoinPresenter presenter;
+    private UserJoinUseCase useCase;
+    private UserJoinOutputPort outputPort;
+    private UserJoinViewModel viewModel;
+    private final User user = User.createInstance(100837, "userjoin", "usecasetest", 100837);
+
+    @Before
+    public void setUp() {
+        this.gateway.setUserId(this.user.getId());
+        this.gateway.insertUser(this.user.getQueryUserData());
+        this.gateway.createBillTable(this.user.getBillId());
+        this.presenter = new UserJoinPresenter(viewModel1 -> UserJoinUseCaseTest.this.viewModel = viewModel1);
+        this.useCase = new UserJoinUseCase(this.gateway, this.outputPort);
+        this.controller = new UserJoinController(this.useCase);
+
+    }
+
+    @After
+    public void tearDown() {
+        this.gateway.cleanUser(this.user.getQueryUserData());
+    }
+
+    @Test
+    public void testRegisterNewUser() throws InterruptedException {
+        UserJoinViewModel expected = new UserJoinViewModel(true, "Registered successfully.");
+        this.controller.userJoin("thenamedoesnotmatter", "123456");
+
+        Assert.assertEquals(expected, this.viewModel);
+    }
+
+    @Test
+    public void testLoginExistingUser() throws InterruptedException {
+        UserJoinViewModel expected = new UserJoinViewModel(true, "Logged in successfully.");
+        this.controller.userJoin("userjoin", "usecasetest");
+        Assert.assertEquals(expected, this.viewModel);
+    }
+
+    @Test
+    public void testWrongPasswordForLogin() throws InterruptedException {
+        UserJoinViewModel expected = new UserJoinViewModel(false, "Incorrect password " +
+                "or the username already exists.");
+        this.controller.userJoin("userjoin", "293840");
+        Assert.assertEquals(expected, this.viewModel);
+    }
+}
